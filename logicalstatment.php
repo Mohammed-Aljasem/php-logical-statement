fad0da4f338bb510957f09f0baa6e4bacb40c6ca
mohammed-aljasem <moh.m.a;jasem@gmail.com>
2021-01-20 09:02:59 +0200

    19, jan

---
+++ logicalstatment.php
@@ -0,0 +1,57 @@
+<?php
+//=============Q1==============
+// $x = 555;
+// if ($x >= 0 && $x <= 50) {
+//   echo $x * 2.5;
+// } elseif ($x >= 51 && $x <= 150) {
+//   echo $x * 5;
+// } elseif ($x >= 151 && $x <= 250) {
+//   echo $x * 6.2;
+// } elseif ($x >= 251) {
+//   echo $x * 7.5;
+// } else {
+//   echo "Error";
+// }
+
+
+//=============Q2==============
+// $x = 4;
+// $z = 4;
+// $result = 0;
+// $a = '/';
+
+
+// if ($a === "+") {
+//   echo $x + $z;
+// } elseif ($a === "-") {
+//   echo $x - $z;
+// } elseif ($a === "*") {
+//   echo $x * $z;
+// } elseif ($a === "/") {
+//   echo $x / $z;
+// } else {
+//   echo "Error ";
+// }
+
+
+//=============Q3==============
+// $x = 110;
+// $c = 'mohammed';
+// if ($x >= 18 && $x <= 120) {
+//   echo "$c You are ready to vote ";
+// } elseif ($x <= 18 && $x >= 0) {
+//   echo "$c You are not ready  to vote";
+// } else {
+//   echo "$c please enter you right age to check";
+// }
+
+
+//=============Q4==============
+// $x = -150;
+// if ($x >= 0) {
+//   echo "The temperature is more than zero";
+// } elseif ($x <= 0) {
+//   echo "The temperature is less than zero";
+// } else {
+//   echo "please enter you wether number to check";
+// }
